2d3af92343a1f4ead1fa397f00a233c2cf7bffec
Chris Swenor <cswenor@gmail.com>
2023-09-02 09:28:03 -0400

    Checking to see the existing Twitter name to see if it matches their NFD.

--- js/main.js
+++ js/main.js
@@ -1,99 +1,114 @@
 // Base URL
 const baseURL = "https://api.nf.domains/nfd/";
 
+// Function to introduce delay
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 // Attach event listener to form submit
 document.addEventListener("DOMContentLoaded", function() {
   const form = document.getElementById('nfdForm');
   const pickWinnerBtn = document.getElementById('pickWinnerBtn');
-  
-  form.addEventListener('submit', function(event) {
+
+  form.addEventListener('submit', async function(event) {
     event.preventDefault();
-
-
     const filterByTwitter = document.getElementById('filterTwitter').checked;
-
     const inputValue = document.getElementById('nfd').value;
     let url1 = `${baseURL}${inputValue}?view=brief&poll=false&nocache=false`;
 
-    fetch(url1)
-      .then(response => response.json())
-      .then(data1 => {
-        const appID = data1.appID;
-        let url2 = `${baseURL}v2/search?parentAppID=${appID}&limit=200&vproperty=twitter&vvalue=&view=full`;
+    try {
+      let response = await fetch(url1);
+      let data1 = await response.json();
+      const appID = data1.appID;
+      let url2 = `${baseURL}v2/search?parentAppID=${appID}&limit=200&vproperty=twitter&vvalue=&view=full`;
 
-        return fetch(url2);
-      })
-      .then(response => response.json())
-      .then(data2 => {
-        let items = data2.nfds;
+      response = await fetch(url2);
+      let data2 = await response.json();
+      let items = data2.nfds;
+      let filteredItems = [];
 
-        // Apply the Twitter filter only if the checkbox is checked
-        if (filterByTwitter) {
-          items = data2.nfds.filter(item => item.properties && item.properties.verified && item.properties.verified.twitter);
-        }
-        let outputList = '<ul>';
-        
-        for (const item of items) {
-          const name = item.name || "Unknown";
-          const link = `${name}`;
-          if (filterByTwitter) {
-            const twitterHandle = item.properties.verified.twitter || "Unknown";
-            outputList += `<li><a href="https://app.nf.domains/name/${link}" target="_blank">Name: ${name}</a>, Twitter Handle: ${twitterHandle}</li>`;
-          } else {
-            outputList += `<li><a href="https://app.nf.domains/name/${link}" target="_blank">Name: ${name}</a></li>`;
+      if (filterByTwitter) {
+        for (let i = 0; i < items.length; i++) {
+          const item = items[i];
+          if (item.properties && item.properties.verified && item.properties.verified.twitter) {
+            // Introduce delay to avoid rate limiting
+            //await sleep(i * 30);
+
+            const name = item.name;
+            const badgeApiUrl = `${baseURL}badges/${name}`;
+            response = await fetch(badgeApiUrl);
+            let badgeData = await response.json();
+            if (badgeData.twitter) {
+              const twitterName = badgeData.twitter[0].twitterName || '';
+              if (twitterName.toLowerCase() === name.toLowerCase()) {
+                item.twitterName = twitterName;  // Add twitterName to the item
+                filteredItems.push(item);
+              }
+            }
           }
         }
-
-        outputList += '</ul>';
-        document.getElementById('output').innerHTML = outputList;
-
-        // Show the 'Pick a Winner' button
-        pickWinnerBtn.style.display = 'block';
-      })
-      .catch(error => {
-        console.log(error);
-        document.getElementById('output').textContent = "An error occurred while making the API calls.";
-      });
-
-  // Pick a winner when the button is clicked
-pickWinnerBtn.addEventListener('click', function() {
-  
-  const items = document.querySelectorAll('#output li');
-  if (items.length > 0) {
-    let interval;
-    let counter = 0;
-    const maxCycles = 20;  // Number of cycles before picking a winner
-    const cycleSpeed = 100; // Milliseconds per cycle
-
-    interval = setInterval(() => {
-      // Cycle through the list
-      const randomIndex = Math.floor(Math.random() * items.length);
-      const candidate = items[randomIndex].textContent;
-
-      document.getElementById('winnerOutput').innerHTML = `<p>Current Candidate: ${candidate}</p>`;
-      document.getElementById('winnerOutput').scrollIntoView({ behavior: 'smooth' });
-
-      if (counter >= maxCycles) {
-        // Stop cycling and pick a winner
-        clearInterval(interval);
-        const winnerIndex = Math.floor(Math.random() * items.length);
-        const winner = items[winnerIndex].textContent;
-
-        // Display the winner with confetti
-        document.getElementById('winnerOutput').innerHTML = `<h2>The Winner is:</h2> <p>${winner}</p>`;
-        // Scroll to the winnerOutput div to show the winner
-        document.getElementById('winnerOutput').scrollIntoView({ behavior: 'smooth' });
-        confetti({
-          spread: 180,
-          particleCount: 150
-        });
-
-
+      } else {
+        filteredItems = items;
       }
 
-      counter++;
-    }, cycleSpeed);
-  }
-});
-});
+      let outputList = '<ul>';
+      for (const item of filteredItems) {
+        const name = item.name || "Unknown";
+        const link = `${name}`;
+        if (filterByTwitter) {
+          const twitterHandle = item.properties.verified.twitter || "Unknown";
+          const twitterName = item.twitterName || "Unknown";
+          outputList += `<li><a href="https://app.nf.domains/name/${link}" target="_blank">Name: ${name}</a>, Twitter Handle: ${twitterHandle}</li>`;
+        } else {
+          outputList += `<li><a href="https://app.nf.domains/name/${link}" target="_blank">Name: ${name}</a></li>`;
+        }
+      }
+      outputList += '</ul>';
+      document.getElementById('output').innerHTML = outputList;
+      pickWinnerBtn.style.display = 'block';
+
+    } catch (error) {
+      console.log(error);
+      document.getElementById('output').textContent = "An error occurred while making the API calls.";
+    }
+  });
+
+  // Pick a winner when the button is clicked
+  pickWinnerBtn.addEventListener('click', function() {
+    const items = document.querySelectorAll('#output li');
+    if (items.length > 0) {
+      let interval;
+      let counter = 0;
+      const maxCycles = 20;  // Number of cycles before picking a winner
+      const cycleSpeed = 100; // Milliseconds per cycle
+
+      interval = setInterval(() => {
+        // Cycle through the list
+        const randomIndex = Math.floor(Math.random() * items.length);
+        const candidate = items[randomIndex].textContent;
+
+        document.getElementById('winnerOutput').innerHTML = `<p>Current Candidate: ${candidate}</p>`;
+        document.getElementById('winnerOutput').scrollIntoView({ behavior: 'smooth' });
+
+        if (counter >= maxCycles) {
+          // Stop cycling and pick a winner
+          clearInterval(interval);
+          const winnerIndex = Math.floor(Math.random() * items.length);
+          const winner = items[winnerIndex].textContent;
+
+          // Display the winner with confetti
+          document.getElementById('winnerOutput').innerHTML = `<h2>The Winner is:</h2> <p>${winner}</p>`;
+          // Scroll to the winnerOutput div to show the winner
+          document.getElementById('winnerOutput').scrollIntoView({ behavior: 'smooth' });
+          confetti({
+            spread: 180,
+            particleCount: 150
+          });
+        }
+
+        counter++;
+      }, cycleSpeed);
+    }
+  });
 });
